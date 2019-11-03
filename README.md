dc559829dcfb46d983b5604632f1272bb4365e3f
Ariadne Conill <ariadne@dereferenced.org>
2019-11-03 14:12:30 -0600

    remove NIF

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Bindings for lexborisov's [myhtml](https://github.com/lexborisov/myhtml).
     iex> Myhtmlex.decode("<h1>Hello world</h1>")
     {"html", [], [{"head", [], []}, {"body", [], [{"h1", [], ["Hello world"]}]}]}
 
-  Benchmark results (Nif calling mode) on various file sizes on a 2,5Ghz Core i7:
+  Benchmark results (removed Nif calling mode) on various file sizes on a 2,5Ghz Core i7:
 
       Settings:
         duration:      1.0 s
@@ -27,53 +27,6 @@ Bindings for lexborisov's [myhtml](https://github.com/lexborisov/myhtml).
       wikipedia_hyperlink.html 97k        1000   1385.86 µs/op
       w3c_html5.html 131k                 1000   2179.30 µs/op
       github_trending_js.html 341k         500   5686.21 µs/op
-
-## Configuration
-
-The module you are calling into is always `Myhtmlex` and depending on your application configuration,
-it chooses between the underlying implementations `Myhtmlex.Safe` (default) and `Myhtmlex.Nif`.
-
-Erlang interoperability is a tricky mine-field.
-You can call into C directly using native implemented functions (Nif). But this comes with the risk,
-that if anything goes wrong within the C implementation, your whole VM will crash.
-No more supervisor cushions for here on, just violent crashes.
-
-That is why the default mode of operation keeps your VM safe and happy.
-If you need ultimate parsing speed, or you can simply tolerate VM-level crashes, read on.
-
-### Call into C-Node (default)
-
-This is the default mode of operation.
-If your application cannot tolerate VM-level crashes, this option allows you to gain the best of both worlds.
-The added overhead is client/server communications, and a worker OS-process that runs next to your VM under VM supervision.
-
-You do not have to do anything to start the worker process, everything is taken care of within the library.
-If you are not running in distributed mode, your VM will automatically be assigned a `sname`.
-
-The worker OS-process stays alive as long as it is under VM-supervision. If your VM goes down, the OS-process will die by itself.
-If the worker OS-process dies for some reason, your VM stays unaffected and will attempt to restart it seamlessly.
-
-### Call into Nif
-
-If your application is aiming for ultimate parsing speed, and in the worst case can tolerate VM-level crashes, you can call directly into the Nif.
-
-1. Require myhtmlex without runtime
-
-    in your `mix.exs`
-
-        def deps do
-          [
-            {:myhtmlex, ">= 0.0.0", runtime: false}
-          ]
-        end
-
-2. Configure the mode to `Myhtmlex.Nif`
-
-    e.g. in `config/config.exs`
-
-        config :myhtmlex, mode: Myhtmlex.Nif
-
-3. Bonus: You can [open up in-memory references to parsed trees](https://hexdocs.pm/myhtmlex/Myhtmlex.html#open/1), without parsing + mapping erlang terms in one go
 
 ## Contribution / Bug Reports
 
--- lib/myhtmlex.ex
+++ lib/myhtmlex.ex
@@ -10,7 +10,7 @@ defmodule Myhtmlex do
       iex> Myhtmlex.decode("<h1>Hello world</h1>")
       {"html", [], [{"head", [], []}, {"body", [], [{"h1", [], ["Hello world"]}]}]}
 
-  Benchmark results (Nif calling mode) on various file sizes on a 2,5Ghz Core i7:
+  Benchmark results (removed Nif calling mode) on various file sizes on a 2,5Ghz Core i7:
 
       Settings:
         duration:      1.0 s
@@ -27,53 +27,6 @@ defmodule Myhtmlex do
       wikipedia_hyperlink.html 97k        1000   1385.86 µs/op
       w3c_html5.html 131k                 1000   2179.30 µs/op
       github_trending_js.html 341k         500   5686.21 µs/op
-
-  ## Configuration
-
-  The module you are calling into is always `Myhtmlex` and depending on your application configuration,
-  it chooses between the underlying implementations `Myhtmlex.Safe` (default) and `Myhtmlex.Nif`.
-
-  Erlang interoperability is a tricky mine-field.
-  You can call into C directly using native implemented functions (Nif). But this comes with the risk,
-  that if anything goes wrong within the C implementation, your whole VM will crash.
-  No more supervisor cushions for here on, just violent crashes.
-
-  That is why the default mode of operation keeps your VM safe and happy.
-  If you need ultimate parsing speed, or you can simply tolerate VM-level crashes, read on.
-
-  ### Call into C-Node (default)
-
-  This is the default mode of operation.
-  If your application cannot tolerate VM-level crashes, this option allows you to gain the best of both worlds.
-  The added overhead is client/server communications, and a worker OS-process that runs next to your VM under VM supervision.
-
-  You do not have to do anything to start the worker process, everything is taken care of within the library.
-  If you are not running in distributed mode, your VM will automatically be assigned a `sname`.
-
-  The worker OS-process stays alive as long as it is under VM-supervision. If your VM goes down, the OS-process will die by itself.
-  If the worker OS-process dies for some reason, your VM stays unaffected and will attempt to restart it seamlessly.
-
-  ### Call into Nif
-
-  If your application is aiming for ultimate parsing speed, and in the worst case can tolerate VM-level crashes, you can call directly into the Nif.
-
-  1. Require myhtmlex without runtime
-
-      in your `mix.exs`
-
-          def deps do
-            [
-              {:myhtmlex, ">= 0.0.0", runtime: false}
-            ]
-          end
-
-  2. Configure the mode to `Myhtmlex.Nif`
-
-      e.g. in `config/config.exs`
-
-          config :myhtmlex, mode: Myhtmlex.Nif
-
-  3. Bonus: You can [open up in-memory references to parsed trees](https://hexdocs.pm/myhtmlex/Myhtmlex.html#open/1), without parsing + mapping erlang terms in one go
   """
 
   @type tag() :: String.t() | atom()
@@ -89,7 +42,7 @@ defmodule Myhtmlex do
   @type format_flag() :: :html_atoms | :nil_self_closing | :comment_tuple3
 
   defp module() do
-    Application.get_env(:myhtmlex, :mode, Myhtmlex.Nif)
+    Application.get_env(:myhtmlex, :mode, Myhtmlex.Safe)
   end
 
   @doc """
@@ -148,29 +101,5 @@ defmodule Myhtmlex do
   @spec decode(String.t(), format: [format_flag()]) :: tree()
   def decode(bin, format: flags) do
     module().decode(bin, flags)
-  end
-
-  @doc """
-  Returns a reference to an internally parsed myhtml_tree_t. (Nif only!)
-  """
-  @spec open(String.t()) :: reference()
-  def open(bin) do
-    Myhtmlex.Nif.open(bin)
-  end
-
-  @doc """
-  Returns a tree representation from the given reference. See `decode/1` for example output.  (Nif only!)
-  """
-  @spec decode_tree(reference()) :: tree()
-  def decode_tree(ref) do
-    Myhtmlex.Nif.decode_tree(ref)
-  end
-
-  @doc """
-  Returns a tree representation from the given reference. See `decode/2` for options and example output. (Nif only!)
-  """
-  @spec decode_tree(reference(), format: [format_flag()]) :: tree()
-  def decode_tree(ref, format: flags) do
-    Myhtmlex.Nif.decode_tree(ref, flags)
   end
 end
