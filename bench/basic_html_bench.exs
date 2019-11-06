bb0a1c961fb77265de46d2c911db41aef5ec7535
Ariadne Conill <ariadne@dereferenced.org>
2019-11-06 10:32:38 -0600

    rework benchmarks

--- bench/basic_html_bench.exs
+++ bench/basic_html_bench.exs
@@ -1,41 +1,23 @@
 defmodule BasicHtmlBench do
   use Benchfella
 
-  setup_all do
-    html = File.read!("bench/w3c_html5.html")
-    context = {html, Myhtmlex.open(html)}
-    {:ok, context}
-  end
-
   bench "decode" do
     {html, _} = bench_context
-    Myhtmlex.decode(html)
+    :fast_html.decode(html)
   end
 
   bench "decode w/ html_atoms" do
     {html, _} = bench_context
-    Myhtmlex.decode(html, format: [:html_atoms])
+    :fast_html.decode(html, format: [:html_atoms])
   end
 
   bench "decode w/ nil_self_closing" do
     {html, _} = bench_context
-    Myhtmlex.decode(html, format: [:nil_self_closing])
+    :fast_html.decode(html, format: [:nil_self_closing])
   end
 
   bench "decode w/ html_atoms, nil_self_closing" do
     {html, _} = bench_context
-    Myhtmlex.decode(html, format: [:html_atoms, :nil_self_closing])
+    :fast_html.decode(html, format: [:html_atoms, :nil_self_closing])
   end
-
-  bench "decode_tree" do
-    {_, ref} = bench_context
-    Myhtmlex.decode_tree(ref)
-  end
-
-  bench "decode_tree w/ html_atoms" do
-    {_, ref} = bench_context
-    Myhtmlex.decode_tree(ref, format: [:html_atoms])
-  end
-
 end
-
