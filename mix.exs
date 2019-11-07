0d842d82ca16e27b1aad3b000d4942e3d081b14c
Ariadne Conill <ariadne@dereferenced.org>
2019-11-07 10:22:26 -0600

    fast_html 0.9.1.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FastHTML.Mixfile do
   def project do
     [
       app: :fast_html,
-      version: "0.9.0",
+      version: "0.9.1",
       elixir: "~> 1.5",
       deps: deps(),
       package: package(),
